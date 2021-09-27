caf376fbc9d543d3b09b9fae72722cc0acfdaca8
LiamOsler <liam.osler@dal.ca>
2021-09-27 20:30:23 -0300

    incorporated masonry

---
+++ includes/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
+
+
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
--- index.php
+++ index.php
@@ -1,42 +1,74 @@
-<?php
-    //Function to read the CSV file to a multidimensional array :
-    function readCSV($csv){
-        $file = fopen($csv, 'r');
-        while (!feof($file) ) {
-            $line[] = fgetcsv($file, 1024);
-        }
-        fclose($file);
-        return $line;
-    }
+<?php include "includes/head.php"?>
+<div class ="container ">
+        <!-- Load the RSS contents: -->
+        <?php
+            //Function to read the CSV file to a multidimensional array :
+            function readCSV($csv){
+                $file = fopen($csv, 'r');
+                while (!feof($file) ) {
+                    $line[] = fgetcsv($file, 1024);
+                }
+                fclose($file);
+                return $line;
+            }
 
-    //Read a csv with the list of feeds to save locally:
-    $feed_list_csv = 'files/feedlist.csv';
-    $feed_list = readCSV($feed_list_csv);
+            //Read a csv with the list of feeds to save locally:
+            $feed_list_csv = 'files/feedlist.csv';
+            $feed_list = readCSV($feed_list_csv);
 
-    $i = 0;
-    foreach($feed_list as $feed_source){
-        $local_copy_string = "files/" . $feed_list[$i][0] . ".xml";
-        $local_copy_string = str_replace(" ", "_", $local_copy_string);
+            //Create a counter keep track of items:
+            $i = 0;
 
-        $rss_feed = simplexml_load_file($local_copy_string);
+            //Loop through each feed list item (each listed rss feed in the csv file):
+            foreach($feed_list as $feed_source){
+                $j = 0;
+                //Generate a string with the file path to the local copy of the RSS feed:
+                $local_copy_string = "files/" . $feed_list[$i][0] . ".xml";
+                $local_copy_string = str_replace(" ", "_", $local_copy_string);
+
+                //Load the RSS feed:
+                $rss_feed = simplexml_load_file($local_copy_string);
+                ?>
+                <!-- Echo the title of the feed (from the CSV file) -->
+                <div class = "row">
+                    <h1>
+                        <?php echo $feed_list[$i][0] ?>
+                    </h1>
+                </div>
+
+
+                <div class = "row " data-masonry='{"percentPosition": true }'>
+                <?php
+                //Go through each item in the feed and log it to the page:
+                foreach ($rss_feed->channel->item as $feed_item) {?> 
+                    <div class = "rss-item col-lg-4 col-4 col-md-6 col-sm-6">
+                        <div class="card border-primary">
+                            <div class="card-body">
+                            <!-- Feed item title formatted as a heading/link -->
+                            <p>
+                                <a href="<?php echo $feed_item->link?>"><?php echo$feed_item->title?></a>
+                            <p>
+
+                            <!-- Feed item description -->
+                            <p>
+                                <?php echo $feed_item->description?>
+                            </p>
+                        </div>
+                    </div>
+                    </div>
+                <?php
+                    $j++;
+                    ?>
+                <?php
+                }
+                echo "</div>";
+                //Close foreach ($rss_feed->channel->item as $feed_item)
+                //Increment the feed counter:
+                $i++;
+            } //Close foreach($feed_list as $feed_source)
         ?>
-        <h1>
-        <?php echo $feed_list[$i][0] ?>
-        </h1>
-        <?php
-        foreach ($rss_feed->channel->item as $feed_item) {
-            ?> 
-            <h2>
-                <a href="<?php echo $feed_item->link?>"><?php echo$feed_item->title?></a>
-            </h2>
-            <p>
-                <?php echo $feed_item->description?>
-            </p>
-        
+    </div>
 
-        <?php
-        
-        }
-        $i++;
-    }
-?>
+
+
+<?php include "includes/foot.php"?>
