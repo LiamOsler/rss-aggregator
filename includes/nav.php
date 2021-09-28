6c2e4b8c470510f4f2c60f5098cbd618588d1b72
LiamOsler <liam.osler@dal.ca>
2021-09-28 02:03:56 -0300

    restyle navbar

--- includes/nav.php
+++ includes/nav.php
@@ -1,9 +1,29 @@
-<nav class="navbar">
-    <ul>
-        <li><a href="#cbc">CBC</a></li>
-        <li><a href="#ctv">CTV</a></li>
-        <li><a href="#global">Global News</a></li>
-        <li><a href="#coast">The Coast</a></li>
-        <li><a href="#coast">The Halifax Examiner</a></li>
-    </ul>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Publisher </a>
+          <ul class="dropdown-menu">
+            <li><a href="#">CBC</a></li>
+            <li><a href="#">CTV</a></li>
+            <li><a href="#">Global</a></li>
+            <li><a href="#">The Coast</a></li>
+          </ul>
+        </li>
+        <li><a href="#"></a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      </ul>
+    </div>
+  </div>
 </nav>
